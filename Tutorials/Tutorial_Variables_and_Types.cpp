3d07602a3cee5e9ca5589709966f3f5710950d79
Bernardo Pla <bpla2112@gmail.com>
2017-06-06 16:38:36 -0400

    tutorial of variables and types

---
+++ Tutorials/Tutorial_Variables_and_Types.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/*
+ * Code teaches how variables are declared.
+ * Uses types of int, float, and double.
+ * Variable sum is the sum of a, b, and c.
+ * Try playing around with this and see what happens when you change the types.
+ */
+int main() {
+    int a = 3;
+    float b = 4.5;
+    double c = 5.25;
+
+    //calculate the sum and print it
+    float sum = a + b + c;
+    printf("The sum of a, b, and c is %f.", sum);
+}
+
+/*
+ * Source: http://www.learn-cpp.org/en/Variables_and_Types
+ */
